786bad986c64a639ed5ca40a6ae6302eb09ab819
harokku <crenna.simone@gmail.com>
2019-03-09 12:03:30 +0100

    Base skeleton with bulma configured, sass imported and hero placeholder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import logo from './base.png';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section className="hero is-danger is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <figure className="image is-128x128 is-inline-block">
+              <img src={logo} alt="CAMPia Logo"/>
+            </figure>
+            <h1 className="title">
+              CAMPia prospettiva
+            </h1>
+            <h2 className="subtitle">
+              Omni Management Console
+            </h2>
+          </div>
+        </div>
+      </section>
     );
   }
 }
